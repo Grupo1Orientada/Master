df4db8746b51f4e000acbdd2ff50246de3f5fbc7
cindygonsales.25@gmail.com <>
2020-11-17 22:52:00 -0600

    La Clase Persona que es la Super Clase y la Clase Doctor Fue Creada Por mi. Cindy Gonzalez

---
+++ src/tareadegrupo/Doctor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareadegrupo;
+
+import Persona.Persona;
+
+/**
+ *
+ * @author CINDY GONZALEZ
+ */
+public class Doctor extends Persona{
+     public Doctor() {
+        
+        setNombre("CINDY GONZALEZ");
+        setEdad(23);
+        setTrabajo("Hospital Caribe");
+    
+}
+    
+}
